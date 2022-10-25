684bdb375e1b0881ff6ceed7773b7822538fc335
as-dodo <cestanastasia@gmail.com>
2022-10-25 17:12:05 +0300

    some small  changes

--- HW_Lesson5.py
+++ HW_Lesson5.py
@@ -1,37 +1,39 @@
+from datetime import datetime
+
 class Song:
     def __init__(self, name, genre, artist, release_year):
         self.name = name
         self.__genre = genre
         self.artist = artist
         self.release_year = release_year
-        print('Song created')
+        print('Song is created')
 
     def get_genre(self):
         print(f'Music genre is {self.__genre}')
+        return self.__genre
 
     def set_genre(self, new_genre):
         self.__genre = new_genre
 
     def __str__(self):
-        return f'The song named {self.name}, genre - {self.__genre}, is sung by {self.artist}, year of release is {self.release_year}'
+        return f'The song named {self.name}, genre - {self.__genre}, is sung by {self.artist}, year of release is {self.release_year}.'
 
 
 
 song60 = Song('Gimme Shelter', 'Rock', 'The Rolling Stones', 1969)
-# print(song60)
-# song60.get_genre()
-# song60.set_genre("Rock'n'roll")
-# song60.get_genre()
+print(song60)
+song60.get_genre()
+song60.set_genre("Rock'n'roll")
+song60.get_genre()
 
 class HitSong(Song):
 
     def __init__(self, name, genre, artist, release_year, album):
         super().__init__(name, genre, artist, release_year)
         self.album = album
-        print('Single is created')
+        print('Song is created')
 
     def count_years(self):
-        from datetime import datetime
         current_year = datetime.now().year
         years = current_year - self.release_year
         return f"The hit song named {self.name} is {years} years old."
